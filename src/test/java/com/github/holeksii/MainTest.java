e34e9ac208684a1e9682f4614ab60c6868bd8d43
Oleksii Hytsiv <oleksii.hytsiv@gmail.com>
2023-02-08 16:39:39 +0200

    Initial commit

---
+++ src/test/java/com/github/holeksii/MainTest.java
@@ -0,0 +1,13 @@
+package com.github.holeksii;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+  @Test
+  void sampleTest() {
+    assertTrue(true);
+  }
+}
